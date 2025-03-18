da6027a676bb3fc2731b9148dd9b2570459c3025
Pablo Isaac Sponchiado da Silva <pablostz2019@gmail.com>
2025-03-18 14:34:40 -0300

    Delete nomesdedragao.js

--- nomesdedragao.js
+++
@@ -1,6 +0,0 @@
-function gerarNomeDeDragao() {
-    var partes = ['Dra', 'Zor', 'Thal', 'Mir', 'Gar', 'Xan', 'Oth', 'Ryn'];
-    return partes[Math.floor(Math.random() * partes.length)] +
-        partes[Math.floor(Math.random() * partes.length)];
-}
-console.log(gerarNomeDeDragao());
