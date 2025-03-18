2624b2b0d9f54f7c0760e61d20c2798621d00914
Pablo Isaac Sponchiado da Silva <pablostz2019@gmail.com>
2025-03-18 14:35:00 -0300

    Delete simuladordedecisao.js

--- simuladordedecisao.js
+++
@@ -1,6 +0,0 @@
-var tomarDecisao = function (pergunta) {
-    var respostas = ["Sim", "Não", "Talvez", "Depende"];
-    var decisao = respostas[Math.floor(Math.random() * respostas.length)];
-    return "Pergunta: \"".concat(pergunta, "\" - Decis\u00E3o: ").concat(decisao);
-};
-console.log(tomarDecisao("Devo viajar este ano?"));
