03b2f05a54f04dc69ee73edc7f29f060a034987c
Pablo Isaac Sponchiado da Silva <pablostz2019@gmail.com>
2025-03-18 14:34:48 -0300

    Delete geradordepoemas.js

--- geradordepoemas.js
+++
@@ -1,7 +0,0 @@
-var substantivos = ['sol', 'lua', 'vento', 'mar'];
-var adjetivos = ['brilhante', 'triste', 'furioso', 'calmo'];
-var verbos = ['dança', 'canta', 'grita', 'sussurra'];
-function gerarPoema() {
-    return "O ".concat(substantivos[Math.floor(Math.random() * substantivos.length)], " ").concat(verbos[Math.floor(Math.random() * verbos.length)], " ").concat(adjetivos[Math.floor(Math.random() * adjetivos.length)], ".");
-}
-console.log(gerarPoema());
